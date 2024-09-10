aa0ec13f33d8b95e78cfc3a7a4580bd021459032
Collin Pfeifer <collinpfeifer@Icloud.com>
2024-09-09 20:31:02 -0400

    added process for installing package, next looking to find out what package manager and then find router in nextjs

--- package/index.js
+++ package/index.js
@@ -7,4 +7,22 @@ const options = yargs.usage("Usage: -i <package>").option("i", {
 
 console.log("Hello" + options.package);
 
+const install = spawn('npm', ['install', 'uploadthing']);
+
+install.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+install.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+install.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+// I need to find out what package manager this project is using, npm, pnpm, yarn, bun, and install uploadthing with the respective packager manager
+// Then make sure it installed
+// I need to install uploadthing and then find out whether the next.js app is with pages or router, from there add in a page for uploadthing with some quick boilerplate. and that should be good
+// future, i would like to add more verbose colors, logs, and options into what projects it can be added into and ofc more packages that it can install
 
