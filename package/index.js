10a799e13bbd8edefe3d16c0ec622f55a78d0cb8
Collin Pfeifer <collinpfeifer@Icloud.com>
2024-09-11 20:34:21 -0400

    added in functionality to search for nextjs router system, find whether typescript is used, find the package manager being used, install the package, next making sure to not overwrite a file and for building out a page component

--- package/index.js
+++ package/index.js
@@ -1,26 +1,80 @@
 import { spawn } from 'node:child_process';
-import yargs from 'yargs';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+import fs from 'node:fs';
 
-const options = yargs.usage("Usage: -i <package>").option("i", {
+const options = yargs(hideBin(process.argv)).usage("Usage: -i <package>").option("i", {
   alias: "package", describe: "The package you want to install", type: "string", demandOption: true
 }).argv;
 
 console.log("Hello" + options.package);
+// Need to find the working directory where the package.json file
+const workingDir = process.cwd();
+console.log(workingDir);
 
-const install = spawn('npm', ['install', 'uploadthing']);
+const files = fs.readdirSync(workingDir)
 
-install.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
+let packageManager = "";
 
-install.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
+for (const file of files){
+  console.log(file);
+  switch(file) {
+    case 'package-lock.json':
+      packageManager = "npm";
+      break;
+    case 'pnpm-lock.yaml':
+      packageManager = "pnpm";
+      break;
+    case 'bun.lockb':
+      packageManager = 'bun';
+      break;
+    case 'yarn.lock':
+      packageManager = "yarn";
+      break;
+    default:
+      packageManager = "npm";
+      break;
+  }
+}
 
-install.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+console.log(packageManager);
 
+const install = spawn(packageManager, ['install', 'uploadthing']);
+
+// figure out what package manager this person users
+//
+//install.stdout.on('data', (data) => {
+//  console.log(`stdout: ${data}`);
+//});
+//
+//install.stderr.on('data', (data) => {
+//  console.error(`stderr: ${data}`);
+//});
+//
+//install.on('close', (code) => {
+//  console.log(`child process exited with code ${code}`);
+//});
+
+// check for nextjs package and then check for the app and pages folder to look for a palce to add the pages components
+// then log out the URL to go to it and try it out.
+// if nextjs isnt installed then log out an error like "Sorry this only works on Next.JS at the moment!"
+let typescriptUsed = false;
+
+for(const file of files) {
+  if(file === 'tsconfig.json') {
+    typescriptUsed = true;
+  }
+}
+
+for(const file of files) {
+ if(fs.lstat(file).isDirectory() && (file === 'app' || file === 'pages')){
+   // first get the name of the directory if it exists
+   const routerName = file;
+   // check to make sure this doesn't overwrite any other file that is in the directory and make sure that tsx or jsx files exist 
+   // check if there is a tsconfig.json file in the root directory
+   const fileName = `uploadthing.${typescriptUsed ? 't': 'j'}sx`;
+ } 
+}
 // I need to find out what package manager this project is using, npm, pnpm, yarn, bun, and install uploadthing with the respective packager manager
 // Then make sure it installed
 // I need to install uploadthing and then find out whether the next.js app is with pages or router, from there add in a page for uploadthing with some quick boilerplate. and that should be good
